81bf319c5c1999d64fb6fd66bc3100f0a3bfae12
Blackbird0_o <arduino.delloye@gmail.com>
2020-01-29 20:20:36 +0100

    Added nav collapse and jump to section

--- parallax.js
+++ parallax.js
@@ -41,9 +41,46 @@ window.addEventListener('scroll', function () {
   parallax('#fast', window.scrollY - 700, -0.3); // - 700 is here to compensate for all the stuff before one arrives at the projects section
   parallax('#slowest', window.scrollY - 700, 0.33);
   parallax('#faster', window.scrollY - 700, -0.4);
-  //console.log(window.scrollY);
   //parallax('#slower', window.scrollY, -0.5);
 });
+
+
+
+/*
+ * Takes care of navigation smooth scrolling
+ */
+
+// Select all nav-links
+nav_links = document.querySelectorAll(".nav-link")
+
+// Add click ecent listener
+for (var i = 0; i < nav_links.length; i++) {
+  nav_links[i].addEventListener("click", () => {
+    document.getElementById('hamb-btn').click(); // collapses the nav menu
+    // ? window.scroll(0, 0); 
+
+    // document.querySelector('a[href*=#]').addEventListener('click', function (e) {
+    //   e.preventDefault(); // prevent hard jump, the default behavior
+
+    //   document.getElementById("#projects").scrollIntoView();
+
+    //   // var target = this.attr("href"); // Set the target as variable
+
+    //   // // perform animated scrolling by getting top-position of target-element and set it as scroll target
+    //   // document.querySelector('html, body').stop().animate({
+    //   //   scrollTop: $(target).offset().top
+    //   // }, 600, function () {
+
+    //   //   location.hash = target; //attach the hash (#jumptarget) to the pageurl
+    //   // });
+
+    //   return false;
+    // });
+
+  });
+}
+
+
 
 // TODO : Add scroll change to right part when click in menu
 //if (typeof window.orientation !== 'undefined') { ... }
